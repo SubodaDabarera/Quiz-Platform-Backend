471c54a242713103261f5bc1cb2439cab6416690
SubodaDabarera <SubodaDabarera99@gmail.com>
2025-03-15 02:31:43 +0100

    backend socket connection issue fixes

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import 'dotenv/config';
 import { connectDB } from './config/db';
-import { app } from './app';
+import { app, server } from './app';
 
 const PORT = process.env.PORT || 5000;
 
@@ -8,6 +8,6 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 // Start server
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
